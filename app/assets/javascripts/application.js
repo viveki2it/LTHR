7ce1daf53adf3bb14d5077364a1c5d8d1b88b4e3
Vivek <viveki4it@gmail.com>
2019-09-03 21:04:44 +0530

    replaced with new designs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,21 +19,36 @@
 //= require toastr_rails
 //= require owl.carousel.min.js
 
-$(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:false,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          }
-      }
-  })
+// $(document).ready(function(){
+//   $('.owl-carousel').owlCarousel({
+//       loop:true,
+//       margin:10,
+//       nav:false,
+//       responsive:{
+//           0:{
+//               items:1
+//           },
+//           600:{
+//               items:2
+//           },
+//           1000:{
+//               items:3
+//           }
+//       }
+//   })
+// });
+
+$(document).ready(function () {
+    $('.play').click(function (e) {
+        e.preventDefault();
+        var video = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/HcL-2Of-0x0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" frameborder="0" allowfullscreen></iframe>';
+        $('.video-wrapper').html(video);
+        $('.pop-up').show();
+        $('.pop-up').addClass("show-pop");
+    });
+    $('.close').click(function () {
+        $('.video-wrapper').empty();
+        $('.pop-up').removeClass("show-pop");
+        $('.pop-up').hide();
+    });
 });
