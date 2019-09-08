9451798e6f60097f78714fbbf400deb38b7ba9f4
Vivek <viveki4it@gmail.com>
2019-09-08 22:35:38 +0530

    updated redis url

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,3 +1,12 @@
+Split.configure do |config|
+  # config.robot_regex = /my_custom_robot_regex/ # Should set it
+  # config.ignore_ip_addresses << '81.19.48.130' # You should set it using SettingsLogic or something
+  config.db_failover = true # handle redis errors gracefully
+  config.db_failover_on_db_error = proc{|error| Rails.logger.error(error.message) }
+  config.allow_multiple_experiments = true # It's fine for me, but might not for you
+  config.enabled = true
+end
+
 Split::Dashboard.use Rack::Auth::Basic do |username, password|
   username == 'admin' && password == 'p4s5w0rd'
 end
