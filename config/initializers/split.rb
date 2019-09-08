d4f821d0ac69e679724b876f287573a9d5d41149
Vivek <viveki4it@gmail.com>
2019-09-08 22:20:28 +0530

    updated redis url

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,4 +1,4 @@
 Split::Dashboard.use Rack::Auth::Basic do |username, password|
   username == 'admin' && password == 'p4s5w0rd'
 end
-Split.redis = ENV["REDISTOGO_URL"] if ENV["REDISTOGO_URL"]
+Split.redis = ENV["REDIS_URL"] if ENV["REDIS_URL"]
