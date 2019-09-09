80abf3af0ef75fdae585836f24ae3cd8e0a13b08
Vivek <viveki4it@gmail.com>
2019-09-09 07:56:07 +0530

    update split password

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -8,6 +8,6 @@ Split.configure do |config|
 end
 
 Split::Dashboard.use Rack::Auth::Basic do |username, password|
-  username == ENV['SPLIT_USERNAME'] && password == 'SPLIT_PASSWORD'
+  username == ENV['SPLIT_USERNAME'] && password == ENV['SPLIT_PASSWORD']
 end
 Split.redis = ENV["REDIS_URL"] if ENV["REDIS_URL"]
