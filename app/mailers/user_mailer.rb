1c14c442e4681a6571c7111e1a3ed24ad649fad3
Vivek <viveki4it@gmail.com>
2019-09-09 22:53:28 +0530

    fix text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def signup_email(user)
     @user = user
-    @twitter_message = "Ready to elevate your shave? Limited-time chance to win a @LTHR hot shaving machine! (#{ENV['DEFAULT_MAILER_HOST'])"
+    @twitter_message = "Ready to elevate your shave? Limited-time chance to win a @LTHR hot shaving machine! (#{ENV['DEFAULT_MAILER_HOST']})"
 
     mail(:to => user.email, :subject => "Thanks for signing up!")
   end
