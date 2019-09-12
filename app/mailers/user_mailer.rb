bd60f2ec9f3c485d66690e3124722582425b7104
Vivek <viveki4it@gmail.com>
2019-09-12 10:08:27 +0530

    updated share text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def signup_email(user)
     @user = user
-    @twitter_message = "Ready to elevate your shave? Limited-time chance to win a @LTHR hot shaving machine! (#{ENV['DEFAULT_MAILER_HOST']})"
+    @twitter_message = "Ready to elevate your shave? Here is your limited-time chance to win a pre-order LTHR hot shaving machine!"
 
     mail(:to => user.email, :subject => "Thanks for signing up!")
   end
