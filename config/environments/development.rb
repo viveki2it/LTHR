8d4d2602e48088cccd129007ace87619ada27e80
Vivek <viveki4it@gmail.com>
2019-10-23 16:42:33 +0530

    updated the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,14 +59,15 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    #:domain               => 'baci.lindsaar.net',
-    :user_name            => 'toiletappbg@gmail.com',
-    :password             => '!qaz@wsx3',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true
-  }  
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   #:domain               => 'baci.lindsaar.net',
+  #   :user_name            => 'toiletappbg@gmail.com',
+  #   :password             => '!qaz@wsx3',
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true
+  # }
+  config.action_mailer.delivery_method = :letter_opener
 end
