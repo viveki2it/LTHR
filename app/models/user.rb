f0632ece198d5c5d6802d1d806c5d350c712eb70
Vivek <viveki4it@gmail.com>
2019-11-09 08:14:29 +0530

    added validation and fixed play issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,24 @@ class User < ApplicationRecord
   validates :email, presence: true, :uniqueness => {:case_sensitive => false, :message => "Email is already taken"}
   validates :email, 'valid_email_2/email': { mx: true, disposable: true, message: "is not a valid email. Please enter valid email" }
   validates :referral_code, uniqueness: true
+  validate :verify_email
 
   before_create :create_referral_code
   after_create :send_welcome_email
    
   def user_url(root_url)
     root_url + "users/" + referral_code
-  end  
+  end
+  
+  def verify_email
+    if self.valid?
+      mailgun_validate = get_validate(self.email)
+      unless mailgun_validate.dig('result').eql?('deliverable')
+        errors.add(:email, "is not a valid email. Please enter valid email")        
+      end
+    end
+  end
+  
 
   private
 
@@ -22,5 +33,14 @@ class User < ApplicationRecord
   def send_welcome_email
     UserMailer.signup_email(self).deliver_later
   end
+
+  def get_validate(email)
+    url_params = { address: email }
+    public_key = ENV['MAILGUN_API_KEY']
+    validate_url = "https://api.mailgun.net/v4/address/validate"
+    response = RestClient::Request.execute method: :get, url: validate_url, headers: { params: url_params }, user: 'api', password: public_key
+    response_json = JSON.parse response
+    response_json
+  end  
 
 end
